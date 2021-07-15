6fbd240627b4bd47a42733bf2230e6bc8a3c7cb3
lee0507 <2eastbow@gmail.com>
2021-07-16 04:06:03 +0900

    1st

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello MinGyu 7 ^^ !");
+                    await context.Response.WriteAsync("밤 새는게 익숙해 지고 있어요!");
                 });
             });
         }
